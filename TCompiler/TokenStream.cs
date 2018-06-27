95383e347ce18ba120e89c1d53b6ba1bc630af42
elipmoc <relipmoc101@gmail.com>
2018-06-27 12:17:46 +0900

    TokenStreamクラスの作成

--- TCompiler/TokenStream.cs
+++ TCompiler/TokenStream.cs
@@ -8,5 +8,35 @@ namespace TCompiler
 {
     class TokenStream
     {
+        private readonly List<Token> tokenlist = new List<Token>();
+        public int NowIndex { get; private set; }
+
+        //次のトークンへインデックスを進める
+        public void Next() { NowIndex++; }
+
+        //前のトークンへインデックスを戻す
+        public void Prev() { NowIndex--; }
+
+        //任意インデックスにロールバック
+        public void Rollback(int index)
+        {
+            NowIndex = index;
+        }
+
+        //nowindexがさすトークンを得る
+        internal Token Get() { return tokenlist[NowIndex]; }
+
+        internal Token this[int index]
+        {
+            get { return tokenlist[index]; }
+        }
+
+        public int Size { get { return tokenlist.Count; } }
+
+        internal TokenStream(List<Token> tokenlist)
+        {
+            NowIndex = 0;
+            this.tokenlist = tokenlist;
+        }
     }
 }

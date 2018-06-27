8970ec5e3b9030eb099cf2a12f42385712797caa
elipmoc <relipmoc101@gmail.com>
2018-06-27 12:14:03 +0900

    Tokenクラスの作成

--- TCompiler/Token.cs
+++ TCompiler/Token.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace TCompiler
 {
+    enum TokenType
+    {
+        Num,
+        Print,
+        Op,
+        Id,
+        Other
+    }
+
     class Token
     {
+        readonly string str;
+
+        readonly TokenType tokenType;
+
+        public Token(string str, TokenType tokenType)
+        {
+            this.str = str;
+            this.tokenType = tokenType;
+        }
+
+        public string Str { get { return str; } }
+
+        public TokenType TokenType { get { return tokenType; } }
+
+        public int GetNum()
+        {
+            if (tokenType != TokenType.Num)
+                throw new Exception("ばーか");
+            return int.Parse(str);
+        }
+
+        public void DebugPrint()
+        {
+            Console.WriteLine("STR=" + str + " : Type" + tokenType.ToString());
+        }
+
     }
 }

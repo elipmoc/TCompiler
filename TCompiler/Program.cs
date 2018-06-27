b1f1db36e4458f9b64a1e622717c55767b68d9ce
elipmoc <relipmoc101@gmail.com>
2018-06-27 11:24:13 +0900

    first commit

--- TCompiler/Program.cs
+++ TCompiler/Program.cs
@@ -10,6 +10,27 @@ namespace TCompiler
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0) return;
+            switch (args[0]){
+                case "-v":
+                case "-V":
+                case "--Version":
+                case "--version":
+                case "-version":
+                case "-Version":
+                    Console.WriteLine("TCompiler version 0.0.1");
+                    break;
+                case "-h":
+                    Console.WriteLine("コンパイル");
+                    Console.WriteLine("tc <コンパイル対象のファイル名> <コンパイル後の名前>");
+                    Console.WriteLine();
+                    Console.WriteLine("バージョン情報");
+                    Console.WriteLine("tc -v or -V or --version or --Version -version -Version");
+                    break;
+                default:
+                    Console.WriteLine("ha?");
+                    break;
+            }
         }
     }
 }
---
+++ TGUI/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var process = new Process();
+            process.StartInfo.FileName=
+                @"C:\Users\b7306\Desktop\2A32KS古谷颯\TCompiler\TCompiler\bin\Debug\tc.exe";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.Arguments = "-h";
+            process.Start();
+            var io = process.StandardOutput;
+            textBox1.Text = io.ReadToEnd();
+            process.WaitForExit();
+            process.Close();
+
+        }
+    }
+}

bc27ad994bf287978751f2933e19623045b5c5c7
elipmoc <relipmoc101@gmail.com>
2018-06-27 16:07:44 +0900

    Result型を雑に実装

---
+++ TCompiler/Result.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCompiler
+{
+    class Result<T,F>
+    {
+        public delegate void OkFunc(T ok);
+        public delegate void ErrFunc(F err);
+        T ok;
+        F err;
+        bool okFlag;
+
+        public static Result<T,F> Err(F err)
+        {
+            return new Result<T, F>(err);
+        }
+        public static Result<T, F> Ok(T ok)
+        {
+            return new Result<T, F>(ok);
+        }
+        private Result(T ok)
+        {
+            this.ok = ok;
+            okFlag = true;
+        }
+
+        private Result(F err)
+        {
+            this.err = err;
+            okFlag = false;
+        }
+
+        public void Match(OkFunc okF,ErrFunc errF)
+        {
+            if (okFlag)
+                okF(ok);
+            else
+                errF(err);
+        }
+    }
+
+}
